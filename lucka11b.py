fae413afc515fcad75f0f925c0ffe5e3bec9a9fb
Cale547 <119942117+Cale547@users.noreply.github.com>
2024-11-11 21:04:31 +0100

    11 all done, 11b is generalized so it can do the first part as well (much cleaner)

--- lucka11b.py
+++ lucka11b.py
@@ -8,64 +8,55 @@ def prettyPrint(input):
 FILENAME = "L11.txt"
 with open(FILENAME, encoding="UTF8") as f:
     INPUT = f.readlines()
-    #f.seek(0)
-    #INPUT_CLONE = f.readlines()
-INPUT_CLONE = []
 
 COLS_TO_ADD = []
 for i,ROW in enumerate(INPUT):
     INPUT[i] = ROW.strip()
-    INPUT_CLONE.append(ROW.strip())
-    COLS_TO_ADD.append(i)
+    COLS_TO_ADD.append(i) # This would not work if the input wasn't square. To fix, just iterate through rows methinks
 print("We start with",len(INPUT),"rows and",len(INPUT[0]),"characters per row.")
 #prettyPrint(INPUT)
 
 
-ROWS_ADDED = 0
-GALAXY_INDEX = 1
+GALAXY_ROWS = []
+GALAXY_COLS = []
+ROWS_TO_ADD = []
 for i,ROW in enumerate(INPUT):
     ADD_ROW = True
     for j,CHAR in enumerate(ROW):
         if CHAR == '#':
-            #INPUT_CLONE[i+ROWS_ADDED] = INPUT_CLONE[i+ROWS_ADDED][:j] + str(GALAXY_INDEX) + INPUT_CLONE[i+ROWS_ADDED][j+1:]
-            #GALAXY_INDEX += 1
+            GALAXY_ROWS.append(i)
+            GALAXY_COLS.append(j)
             ADD_ROW = False
             if j in COLS_TO_ADD:
                 COLS_TO_ADD.remove(j)
     if ADD_ROW:
-        INPUT_CLONE.insert(i+ROWS_ADDED,"."*len(INPUT_CLONE[0]))
-        ROWS_ADDED += 1
-
-INPUT = INPUT_CLONE.copy()
-for i,ROW in enumerate(INPUT):
-    START = 0
-    TEMP = ""
-    for COL in COLS_TO_ADD:
-        TEMP += ROW[START:COL]
-        TEMP += "."
-        START = COL
-    TEMP += ROW[START:]
-    INPUT[i] = TEMP
-print("We end with",len(INPUT),"rows and",len(INPUT[0]),"characters per row.")
-#prettyPrint(INPUT)
-
+        ROWS_TO_ADD.append(i)
 
 # using enumerate ends at i,j = 11,12 and gives the correct result
 # using range(len(INPUT/ROW)) ends at i,j = 11,9 and fucks up
-GALAXY_ROWS = []
-GALAXY_COLS = []
-for i,ROW in enumerate(INPUT):
-    for j,COL in enumerate(ROW):
-        if INPUT[i][j] != ".":
-            GALAXY_ROWS.append(i)
-            GALAXY_COLS.append(j)
 
+print("We end with",len(INPUT)+len(ROWS_TO_ADD),"rows and",len(INPUT[0])+len(COLS_TO_ADD),"characters per row.")
+#prettyPrint(INPUT)
+
+EXTRA_SPACE = 1000000 - 1
 shortestDistSum = 0
-for i in range(len(GALAXY_ROWS)):
-    for j in range(i+1,len(GALAXY_ROWS)):
-        #print(i,j)
-        shortestDistSum += abs(GALAXY_ROWS[i]-GALAXY_ROWS[j]) + abs(GALAXY_COLS[i]-GALAXY_COLS[j])
+nGalaxies = len(GALAXY_ROWS)
+for i in range(nGalaxies):
+    for j in range(i+1,nGalaxies):
+        ROWS_APART = 0
+        COLS_APART = 0
+        for row in ROWS_TO_ADD:
+            if (GALAXY_ROWS[i] < row < GALAXY_ROWS[j]) or (GALAXY_ROWS[j] < row < GALAXY_ROWS[i]):
+                ROWS_APART += 1
+        for col in COLS_TO_ADD:
+            if (GALAXY_COLS[i] < col < GALAXY_COLS[j]) or (GALAXY_COLS[j] < col < GALAXY_COLS[i]):
+                COLS_APART += 1
+
+        
+
+        shortestDistSum += abs(GALAXY_ROWS[i]-GALAXY_ROWS[j]) + abs(GALAXY_COLS[i]-GALAXY_COLS[j]) + EXTRA_SPACE*(ROWS_APART+COLS_APART)
 print("Sum of all shortest distances between galaxies are:",shortestDistSum)
+
 
 # 105265514 is too high
 # **REDACTED** is just right
