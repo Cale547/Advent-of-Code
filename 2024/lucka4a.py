adaebe7745cb55dfa20ca25871c70b025ae56b41
Cale547 <119942117+Cale547@users.noreply.github.com>
2024-12-04 09:32:01 +0100

    start_diagonals start at correct value, lists are iterated through together

--- 2024/lucka4a.py
+++ 2024/lucka4a.py
@@ -1,4 +1,5 @@
 import re
+from itertools import zip_longest
 
 FILENAME = "2024/L4.txt"
 with open(FILENAME, encoding="UTF8") as f:
@@ -27,30 +28,26 @@ for row in my_input:
 
 my_diagonals1 = [""]*(2*len(my_input)-1)
 my_diagonals2 = [""]*(2*len(my_input)-1)
-start_diagonal1 = -1
-start_diagonal2 = 2
+start_diagonal1 = 0
+start_diagonal2 = 3
 for i,row in enumerate(my_input):
-    start_diagonal1 += 1
-    start_diagonal2 += 1
     for j,col in enumerate(row):
         my_diagonals1[start_diagonal1+j] += col
         my_diagonals2[start_diagonal2-j] += col
+    start_diagonal1 += 1
+    start_diagonal2 += 1
 
 #print(my_diagonals1)
 #print(my_diagonals2)
-        
 
 xmas_count = 0
-for words in my_rows:
-    xmas_count += len(re.findall("XMAS", words))
-    xmas_count += len(re.findall("SAMX", words))
-for words in my_cols:
-    xmas_count += len(re.findall("XMAS", words))
-    xmas_count += len(re.findall("SAMX", words))
-for words in my_diagonals1:
-    xmas_count += len(re.findall("XMAS", words))
-    xmas_count += len(re.findall("SAMX", words))
-for words in my_diagonals2:
-    xmas_count += len(re.findall("XMAS", words))
-    xmas_count += len(re.findall("SAMX", words))
+for a,b,c,d in zip_longest(my_rows,my_cols,my_diagonals1,my_diagonals2, fillvalue="MMM, donuts"):
+    xmas_count += len(re.findall("XMAS", a))
+    xmas_count += len(re.findall("SAMX", a))
+    xmas_count += len(re.findall("XMAS", b))
+    xmas_count += len(re.findall("SAMX", b))
+    xmas_count += len(re.findall("XMAS", c))
+    xmas_count += len(re.findall("SAMX", c))
+    xmas_count += len(re.findall("XMAS", d))
+    xmas_count += len(re.findall("SAMX", d))
 print("Total occurrences:",xmas_count)
