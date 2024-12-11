ceeb766035a0e262a21d36cc372e5df687db1931
Cale547 <119942117+Cale547@users.noreply.github.com>
2024-12-11 13:41:56 +0100

    älva A

--- 2024/lucka11a.py
+++ 2024/lucka11a.py
@@ -1,19 +1,35 @@
 def blink(stone, blinks):
-    blinks_left = blinks
-    total_stones = 1
-    
-    if stone == '0':
-        
-    
-    if len(stone)
+    #print("Blinking at stone engraved with",stone,"with",blinks,"blinks left.")
+    stone_count = 1
+    digits = len(stone)
+    if blinks == 0:
+        return stone_count
 
+    if int(stone) == 0:
+        stone_count -= 1
+        stone_count += blink('1',blinks-1)
 
+    elif digits % 2 == 0:
+        stone_count -= 1
+        half = int(digits/2)
+        stone1 = str(int(stone[:half])) # Removes leading zeroes from stones
+        stone2 = str(int(stone[half:])) 
 
+        stone_count += blink(stone1, blinks-1) + blink(stone2, blinks-1)
 
+    else:
+        stone_count -= 1
+        new_stone = str(int(stone)*2024)
+        stone_count += blink(new_stone, blinks-1)
+    return stone_count
 
-# get input
+FILENAME = "2024/L11.txt"
+with open(FILENAME, encoding="UTF8") as f:
+    INPUT = f.readlines()
 
-rock_map = INPUT.split()
+rock_map = INPUT[0].split()
 
+rocks_found = 0
 for rock in rock_map:
-    blink(rock, 25)
+    rocks_found += blink(rock, 25)
+print("In the final line,",rocks_found,"rocks lie.")
