d3116f948b4b5b2768aa63f70912857a8d4a943e
Cale547 <119942117+Cale547@users.noreply.github.com>
2024-12-11 08:01:47 +0100

    11a start

---
+++ 2024/lucka11a.py
@@ -0,0 +1,19 @@
+def blink(stone, blinks):
+    blinks_left = blinks
+    total_stones = 1
+    
+    if stone == '0':
+        
+    
+    if len(stone)
+
+
+
+
+
+# get input
+
+rock_map = INPUT.split()
+
+for rock in rock_map:
+    blink(rock, 25)
