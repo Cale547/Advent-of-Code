975064600b71104aa2aa7e0e0d65d5cde7000df3
Cale547 <119942117+Cale547@users.noreply.github.com>
2024-12-13 17:01:23 +0100

    Snyggat till

--- 2024/lucka13b.py
+++ 2024/lucka13b.py
@@ -5,21 +5,16 @@ with open(FILENAME, encoding="UTF8") as f:
     INPUT = f.readlines()
 
 game_rounds = int((len(INPUT)+1)/4)
-
 total_cost = 0
 for i in range(game_rounds):
     a_line = INPUT[4*i]
     b_line = INPUT[4*i+1]
     p_line = INPUT[4*i+2]
-    a = re.findall('\d{1,}', a_line)
-    b = re.findall('\d{1,}', b_line)
-    p = re.findall('\d{1,}', p_line)
-    a[0] = int(a[0])
-    a[1] = int(a[1])
-    b[0] = int(b[0])
-    b[1] = int(b[1])
-    p[0] = int(p[0]) + 10000000000000
-    p[1] = int(p[1]) + 10000000000000
+    a = list(map(int, re.findall(r'\d+', a_line)))
+    b = list(map(int, re.findall(r'\d+', b_line)))
+    p = list(map(int, re.findall(r'\d+', p_line)))
+    p[0] += 10000000000000
+    p[1] += 10000000000000
     possible_sols = []
 
     b_ans = (p[1]*a[0]-a[1]*p[0]) / (b[1]*a[0]-a[1]*b[0])
@@ -27,5 +22,5 @@ for i in range(game_rounds):
 
     if (b_ans > 0 and int(b_ans)/b_ans == 1) and (a_ans > 0 and int(a_ans)/a_ans == 1):
         total_cost += int(3*a_ans+b_ans)
-        
+
 print("Total cost of all possible prizes is:",total_cost)
--- 2024/lucka3a.py
+++ 2024/lucka3a.py
@@ -1,6 +1,6 @@
 import re
 
-FILENAME = "2024/L3ex.txt"
+FILENAME = "2024/L3.txt"
 with open(FILENAME, encoding="UTF8") as f:
     temp_input = f.readlines()
 
@@ -8,14 +8,14 @@ my_input = ""
 for line in temp_input:
     my_input += line.strip()
 
-operations = re.findall("mul\([0-9]+?,[0-9]+?\)", my_input)
+operations = re.findall("mul\(\d+,\d+\)", my_input)
 print(operations)
 
 
 mul_result = 0
 for op in operations:
     temp = op.split(",")
-    num1 = re.findall("[0-9]+",temp[0])[0]
-    num2 = re.findall("[0-9]+",temp[1])[0]
+    num1 = re.findall("\d+",temp[0])[0]
+    num2 = re.findall("\d+",temp[1])[0]
     mul_result += int(num1)*int(num2)
 print("Result of all mul operations:",mul_result)
