c6b8306c12ffad59923817e2755ce114c79e5eb9
Cale547 <119942117+Cale547@users.noreply.github.com>
2024-12-14 01:16:15 +0100

    hädanefter TUPLES för att spara besökta positioner!!!

--- 2024/lucka8b.py
+++ 2024/lucka8b.py
@@ -1,13 +1,3 @@
-def myCopy(fromList):
-    toList = []
-    for k,localRow in enumerate(fromList):
-        row_copy = []
-        for l,localCol in enumerate(localRow):
-            row_copy.append(localCol)
-        toList.append(row_copy)
-    return toList
-
-
 FILENAME = "2024/L8.txt"
 with open(FILENAME, encoding="UTF8") as f:
     INPUT = f.readlines()
@@ -25,13 +15,10 @@ for i,row in enumerate(ant_map):
     for j,current in enumerate(row):
         if current == '.':
             continue
-
-        antenna_type = current
         ant_rows.append(i)
         ant_cols.append(j)
 
 
-clone_map = myCopy(ant_map)
 antinodes = 0
 tagged = []
 for i in range(len(ant_rows)-1):
@@ -42,47 +29,29 @@ for i in range(len(ant_rows)-1):
         col2 = ant_cols[j]
         if ant_map[row1][col1] != ant_map[row2][col2]:
             continue
+
         row_diff = row1-row2
         col_diff = col1-col2
 
-        match_found = True
         extender = 0
-        while match_found:
-            match_found = False
+        while extender < 100:
             pot_row1 = row1 + row_diff*extender
             pot_col1 = col1 + col_diff*extender
-            
-            pot_row2 = row2 - row_diff*extender
-            pot_col2 = col2 - col_diff*extender
 
-            """ if row1 == 2 and col1 == 5:
-                print(antinodes)
-                print(pot_row1,row1,pot_col1,col1)
-                print(pot_row2,row2,pot_col2,col2) """
+            pot_row2 = row1 - row_diff*extender
+            pot_col2 = col1 - col_diff*extender
 
             if 0 <= pot_row1 < len(ant_map) and 0 <= pot_col1 < len(ant_map[0]):
-                match_found = True
-                if str(pot_row1)+str(pot_col1) not in tagged:
-                    if clone_map[pot_row1][pot_col1] == '.':clone_map[pot_row1][pot_col1] = '#'
-                    #print("Antinode found at[",pot_row1,pot_col1,"] from antennas",row1,col1,"+",row2,col2)
+                if ((pot_row1,pot_col1)) not in tagged:
                     antinodes += 1
-                tagged.append(str(pot_row1)+str(pot_col1))
+                    tagged.append((pot_row1,pot_col1))
+
             if 0 <= pot_row2 < len(ant_map) and 0 <= pot_col2 < len(ant_map[0]):
-                match_found = True
-                if str(pot_row2)+str(pot_col2) not in tagged:
-                    if clone_map[pot_row2][pot_col2] == '.':clone_map[pot_row2][pot_col2] = '#'
-                    #print("Antinode found at[",pot_row2,pot_col2,"] from antennas",row1,col1,"+",row2,col2)
+                if ((pot_row2,pot_col2)) not in tagged:
                     antinodes += 1
-                tagged.append(str(pot_row2)+str(pot_col2))
+                    tagged.append((pot_row2,pot_col2))
             extender += 1
+        #print(row1,col1)
+        #print(row2,col2,temp_found)
 
 print("Total number of antinodes:",antinodes)
-# 1011 is too low
-
-
-with open("2024/testMap.txt","w",encoding="UTF8") as f:
-    for r in clone_map:
-        toWrite = ''.join(r)+'\n'
-        f.write(toWrite)
-print(ant_rows)
-print(ant_cols)
